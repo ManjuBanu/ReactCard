2a6686e836ad6433327ffb0bca1159477948e9bb
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-09 17:22:37 +0530

    first-commit-9-6-20

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {MyForm} from './form-table';
+import { UpdateTime } from './virtual_dom';
+import {Mounting} from './mounting';
+import {ApiAxios} from './apicall';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import NotFound from './notFoundPage';
+import styled from 'styled-components';
+import {Routing}  from './routing';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.Fragment>
+  <App />
+  <Routing/>
+  {/* <MyForm/>
+  <UpdateTime/>
+ <Mounting/> 
+<ApiAxios/> */}
+</React.Fragment>,
+document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
